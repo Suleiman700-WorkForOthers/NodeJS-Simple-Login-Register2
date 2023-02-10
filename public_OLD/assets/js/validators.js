462213446a062e218f11569646201fbb38675b64
Suleiman <soleman630@gmail.com>
2023-02-11 01:14:38 +0200

    Updates

--- public_OLD/assets/js/validators.js
+++
@@ -1,9 +0,0 @@
-/**
- * receive email and validate it
- * @param _email {string} example: user@domain.com
- * @return {boolean}
- */
-function validateEmail(_email) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(_email)) return true
-    else return false
-}
